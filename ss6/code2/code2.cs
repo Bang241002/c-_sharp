3dfbe9e8863569e176fcd3bf939a4637b0e027b6
Bang241002 <bang241002@gmail.com>
2022-07-01 22:16:45 +0700

    ss6

---
+++ ss6/code2/code2.cs
@@ -0,0 +1,21 @@
+﻿class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Every animal eats something");
+    }
+    public void DoSomething()
+    {
+        Console.WriteLine("Every animal does something");
+    }
+}
+
+class Cat : Animal
+{
+    static void Main(String[] args)
+    {
+        Cat objcat = new Cat();
+        objcat.Eat();
+        objcat.DoSomething();
+    }
+}
---
+++ ss6/ss6/code1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+    class Animal
+{
+    public void Eat() {
+        Console.WriteLine("Evaery animal eats something");
+    }
+}
+
+class Mamnal : Animal
+{
+    public void Feature()
+    {
+        Console.WriteLine("Mammals give birth to young ones ");
+    }
+}
+
+class Dog : Mamnal
+{
+    public void Noise()
+    {
+        Console.WriteLine("dogBarks");
+    }
+    static void Main(string[] args)
+    {
+        Dog objDog = new Dog();
+        objDog.Eat();
+        objDog.Feature();
+        objDog.Noise();
+    }
+}
